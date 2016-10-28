4d0b7764bb7e9f714122212ce195756450116be7
Garrido <U6022960@ten.thomsonreuters.com>
2016-10-29 00:10:01 +0200

    port

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT||3000, function () {
+var server = app.listen(process.env.PORT||8000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
