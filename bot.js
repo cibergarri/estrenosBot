547130ad8ea989a9d83d7546064db27fa4a827c1
Garrido <U6022960@ten.thomsonreuters.com>
2016-10-29 11:45:07 +0200

    opciones

--- bot.js
+++ bot.js
@@ -5,18 +5,23 @@ var token = '290157752:AAHg_tY6doG9LsvsULyyMzDkqVFxiN5VIPw';
 // Setup polling way
 var bot = new TelegramBot(token, {polling: true});
 
-bot.on('text', function (msg) {
+bot.onText(/^\/estrenos/, function (msg) {
   var xhr = new XMLHttpRequest();
   var json_string=httpGet("https://api.themoviedb.org/3/movie/now_playing?api_key=44a2cd8bedba52acd348d4456277bd51&language=es-ES", false);  
   var obj = JSON.parse( json_string );
   var resul="";
   obj.results.forEach(function(item,index){
     resul +=item.title + "\n";
-  })
-
-  
+  });  
   var chatId = msg.chat.id;
   bot.sendMessage(chatId,resul);
+});
+bot.onText(/^\/cines/,function(msg){
+  var chatId = msg.chat.id;
+  bot.sendMessage(chatId,"cines");
+});
+bot.on('text',function(msg){
+
 });
 
 function httpGet(theUrl)
